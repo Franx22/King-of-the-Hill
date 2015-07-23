de1a21da8b57d99d216dea4182191f0690b4922e
Francisco <Francisco@Francisco>
2015-07-23 17:34:02 -0600

    Francisco: Pruebas repositorio3

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         int x = 10;
-        System.out.println(x);
+        System.out.println(x+1);
     }
     
 }
